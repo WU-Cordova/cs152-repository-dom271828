8d8889880422dfdf8ebe415ac3d4017c3b5de39a
Dominic <dombug13@gmail.com>
2025-01-24 12:59:03 -0800

    Bag 1/24/25
    5 tests successful

--- datastructures/bag.py
+++ datastructures/bag.py
@@ -4,8 +4,7 @@ from datastructures.ibag import IBag, T
 
 class Bag(IBag[T]):
     def __init__(self, *items: Optional[Iterable[T]]) -> None:
-        self.__bag = {}
-
+        self.__bag = { }
         if items is not None:
             for item in items:
                 if item in self.__bag: # put in add function
@@ -13,16 +12,27 @@ class Bag(IBag[T]):
                 else:
                     self.__bag[item] = 1
 
-        raise NotImplementedError("__init__ method not implemented")
-
     def add(self, item: T) -> None:
-        raise NotImplementedError("add method not implemented")
+        if item in self.__bag: 
+            self.__bag[item] += 1
+        else:
+            self.__bag[item] = 1
+        if item == None:
+            raise TypeError("NoneType added")
+        # raise NotImplementedError("add method not implemented")
 
     def remove(self, item: T) -> None:
-        raise NotImplementedError("remove method not implemented")
+        if item in self.__bag:
+            self.__bag[item] -= 1
+        else:
+            raise ValueError
+        # raise NotImplementedError("remove method not implemented")
 
     def count(self, item: T) -> int:
-        raise NotImplementedError("count method not implemented")
+        if item in self.__bag:
+            return self.__bag[item]
+        else:
+            return 0
 
     def __len__(self) -> int:
         raise NotImplementedError("__len__ method not implemented")
